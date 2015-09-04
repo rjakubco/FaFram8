17cd5aeb4dca7cf0e9675c368f629eaa88dfb315
Roman Jakubco <rjakubco@redhat.com>
2015-09-04 16:32:59 +0200

    [Remote] Basic test for testing remote fafram

--- deployer/src/test/java/org/jboss/fuse/qa/fafram8/test/remote/RemoteTest.java
+++ deployer/src/test/java/org/jboss/fuse/qa/fafram8/test/remote/RemoteTest.java
@@ -1,7 +1,26 @@
 package org.jboss.fuse.qa.fafram8.test.remote;
 
+import org.jboss.fuse.qa.fafram8.property.FaframConstant;
+import org.jboss.fuse.qa.fafram8.resource.Fafram;
+
+import org.junit.Rule;
+import org.junit.Test;
+
 /**
  * @author : Roman Jakubco (rjakubco@redhat.com)
  */
 public class RemoteTest {
+	static {
+		System.setProperty(FaframConstant.HOST, "10.8.49.84");
+		System.setProperty(FaframConstant.FUSE_ZIP, "http://repository.jboss.org/nexus/content/groups/ea/org/jboss/fuse/jboss-fuse-full/6.2.1.redhat-020/jboss-fuse-full-6.2.1.redhat-020.zip");
+	}
+
+	@Rule
+	public Fafram fafram = new Fafram();
+
+	@Test
+	public void testName() throws Exception {
+		System.out.println("test");
+
+	}
 }
