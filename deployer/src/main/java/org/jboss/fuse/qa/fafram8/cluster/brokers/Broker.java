bc6df95eea18c06029056a6c5b11000c7f03619d
Matej Melko <mmelko@dhcp-10-40-2-123.brq.redhat.com>
2015-12-01 08:09:26 +0100

    [Broker] added broker class

---
+++ deployer/src/main/java/org/jboss/fuse/qa/fafram8/cluster/brokers/Broker.java
@@ -0,0 +1,128 @@
+package org.jboss.fuse.qa.fafram8.cluster.brokers;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+import org.jboss.fuse.qa.fafram8.cluster.Container;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by mmelko on 09/10/15.
+ */
+public class Broker {
+
+	@Getter
+	@Setter
+	private String name;
+
+	@Getter
+	@Setter
+	private boolean ssl;
+
+	@Setter
+	@Getter
+	private String kind;
+
+	@Getter
+	@Setter
+	private String group = "default";
+
+	@Getter
+	@Setter
+	private String data;
+
+	@Getter
+	@Setter
+	private Map<String, Pair<String, String>> networks = new HashMap<String, Pair<String, String>>();
+
+	@Getter
+	@Setter
+	private Container container;
+
+	@Getter
+	@Setter
+	private String parentProfile;
+
+	@Getter
+	@Setter
+	private Map<String, String> pids = new HashMap<>();
+
+	/**
+	 * List of commands needed for creation of the broker.
+	 *
+	 * @return list of commands.
+	 */
+	public List<String> getCreateCommands() {
+		final List<String> commands = new LinkedList<>();
+		commands.add(mqCreate());
+
+		for (Map.Entry<String, String> entry : pids.entrySet()) {
+			commands.add("profile-edit " + mqServerPidModification(entry.getKey(), entry.getValue()) + getProfileName());
+		}
+
+		return commands;
+	}
+
+	/**
+	 * Get name of the profile of the broker.
+	 *
+	 * @return profile.
+	 */
+	public String getProfileName() {
+		return "mq-broker-" + group + "." + name;
+	}
+
+	/**
+	 * Creates mq-create command.
+	 *
+	 * @return mq-create command.
+	 */
+	private String mqCreate() {
+		String cmd = "mq-create ";
+
+		if (!ssl) {
+			cmd += "--no-ssl";
+		}
+		if (kind != null && !"".equals(kind)) {
+			cmd += " --kind " + kind;
+		}
+		if (parentProfile != null && !"".equals(parentProfile)) {
+			cmd += " --parent-profile " + parentProfile;
+		}
+		if (group != null && !"".equals(group)) {
+			cmd += " --group " + group;
+		}
+		if (data != null && !"".equals(data)) {
+			cmd += " --data " + data;
+		}
+
+		for (Map.Entry<String, Pair<String, String>> network : networks.entrySet()) {
+			cmd += " --network " + network.getKey();
+			if (network.getValue() != null) {
+				if (network.getValue().getLeft() != null && !"".equals(network.getValue().getLeft())) {
+					cmd += " --network-username " + network.getValue().getLeft() + " --network-password " + network.getValue().getRight();
+				}
+			}
+		}
+
+		cmd += " " + name;
+		return cmd;
+	}
+
+	/**
+	 * Creates command for broker profile.
+	 *
+	 * @param key key of pid
+	 * @param value value of pid
+	 * @return proper value.
+	 */
+	private String mqServerPidModification(String key, String value) {
+		return "io.fabric8.mq.fabric.server-" + name + "/" + key + "=" + value + " ";
+	}
+}
