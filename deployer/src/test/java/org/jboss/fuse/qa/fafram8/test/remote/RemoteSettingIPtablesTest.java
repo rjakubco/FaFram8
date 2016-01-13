b94566ef0ab731b825147d21a72c037e40c3cf0a
Roman Jakubco <rjakubco@redhat.com>
2016-01-13 11:18:40 +0100

    [Offline] Added tests for setting offline env and loading custom iptables configuration file

--- deployer/src/test/java/org/jboss/fuse/qa/fafram8/test/remote/RemoteSettingIPtablesTest.java
+++ deployer/src/test/java/org/jboss/fuse/qa/fafram8/test/remote/RemoteSettingIPtablesTest.java
@@ -14,6 +14,7 @@ import org.jboss.fuse.qa.fafram8.resource.Fafram;
 import org.jboss.fuse.qa.fafram8.ssh.NodeSSHClient;
 import org.jboss.fuse.qa.fafram8.ssh.SSHClient;
 
+import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
@@ -21,31 +22,38 @@ import org.junit.Rule;
 import org.junit.Test;
 
 /**
- * Test for setting up offline environment when using OpenStack provision.
+ * Test for loading custom iptables configuration file from local to remote machine.
  *
  * @author : Roman Jakubco (rjakubco@redhat.com)
  */
-public class RemoteTurnOffInternetTest {
+public class RemoteSettingIPtablesTest {
 	private ProvisionProvider provider = new OpenStackProvisionProvider();
+	private final static String DIR = "test/working/directory";
 
 	@Rule
-	public Fafram fafram = new Fafram().name("internet-test").provideNodes(provider).withFabric().getBuilder().ssh("ssh-internet").nodeSsh("openstack", "fuse", "fuse").addToFafram().getFafram().offline();
+	public Fafram fafram = new Fafram().name("iptables-test").provideNodes(provider).withFabric().getBuilder().ssh("ssh-iptables").nodeSsh("openstack", "fuse", "fuse").addToFafram().getFafram().loadIPtablesConfigurationFile("target/test-classes/iptables-test");
 
 	@BeforeClass
 	public static void before() {
 		System.setProperty(FaframConstant.FUSE_ZIP, "file:/home/fuse/storage/fuse/jboss-fuse-full-6.2.1.redhat-084.zip");
 	}
 
+	@After
+	public void tearDown() {
+		Fafram.getProvisionProvider().releaseResources();
+		System.clearProperty(FaframConstant.WORKING_DIRECTORY);
+	}
+
 	@AfterClass
 	public static void clean() {
-		Fafram.getProvisionProvider().releaseResources();
 		System.clearProperty(FaframConstant.FUSE_ZIP);
+		Fafram.getProvisionProvider().releaseResources();
 	}
 
 	//TODO(rjakubco): uncomment this when OpenStack is more stable and fafram was refactored
 	@Test
 	@Ignore
-	public void testInternet() throws VerifyFalseException, SSHClientException, KarafSessionDownException {
+	public void testLoadingIptables() throws VerifyFalseException, SSHClientException, KarafSessionDownException {
 		Node rootNode = fafram.getContainerList().get(0).getHostNode();
 		for (Container c : fafram.getContainerList()) {
 			String preCommand = "";
@@ -55,12 +63,11 @@ public class RemoteTurnOffInternetTest {
 
 			SSHClient sshClient = new NodeSSHClient().defaultSSHPort().hostname(rootNode.getHost())
 					.username(rootNode.getUsername()).password(rootNode.getPassword());
-
 			sshClient.connect(true);
 
-			String response = sshClient.executeCommand(preCommand + "wget www.google.com", true);
+			String response = sshClient.executeCommand(preCommand + "sudo iptables -L -n", true);
 
-			assertTrue(response.contains("failed: Connection refused") && response.contains("failed: Network is unreachable."));
+			assertTrue(response.contains("ACCEPT     icmp --  0.0.0.0/0") && response.contains("state NEW tcp dpt:22"));
 		}
 	}
 }
